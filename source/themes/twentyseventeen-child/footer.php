3252c61baeb8012a700f2d40b93a50b98555b3d4
Ben Redden <benjamminredden@gmail.com>
2017-09-09 15:04:16 -0400

    style edits, content placement edits

--- source/themes/twentyseventeen-child/footer.php
+++ source/themes/twentyseventeen-child/footer.php
@@ -51,24 +51,7 @@
   <button class="closeBtn" onclick="closeContactLightbox();"><i class="fa fa-times"></i></button>
   <div class="wrap">
     <h2 style="text-align: center; display: block;" class="bioRhyme">Just Say "YO"!</h2>
-    <form id="contact-form">
-      <input type="hidden" name="action" value="contact_send" />
-      <div class="inputWrap">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name">
-      </div>
-      <div class="inputWrap">
-        <label for="email">Email</label>
-        <input type="text" name="email">
-      </div>
-      <div class="inputWrap">
-        <label for="message">Message</label>
-        <textarea name="message" rows="8" cols="80"></textarea>
-      </div>
-      <button type="submit">
-        SAY YO! <i class="fa fa-circle-o-notch"></i>
-      </button>
-    </form>
+    <?php echo do_shortcode('[contact-form-7 id="59" title="Contact form"]'); ?>
   </div>
 </div>
 
